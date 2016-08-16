ac30f91f07d84cfebeef9385480271fb32670122
Dragos Ct <dragosct10@gmail.com>
2016-08-16 12:58:56 +0300

    add material min.js

--- assets/js/wizard.js
+++ assets/js/wizard.js
@@ -2,28 +2,31 @@ searchVisible = 0;
 transparent = true;
 
 $(document).ready(function(){
+
+    $.material.init();
+
     /*  Activate the tooltips      */
     $('[rel="tooltip"]').tooltip();
-      
+
     $('.wizard-card').bootstrapWizard({
         'tabClass': 'nav nav-pills',
         'nextSelector': '.btn-next',
         'previousSelector': '.btn-previous',
-         
+
          onInit : function(tab, navigation, index){
-            
+
            //check number of tabs and fill the entire row
            var $total = navigation.find('li').length;
            $width = 100/$total;
-           
+
            $display_width = $(document).width();
-           
+
            if($display_width < 600 && $total > 3){
                $width = 50;
            }
-           
+
            navigation.find('li').css('width',$width + '%');
-           
+
         },
         onNext: function(tab, navigation, index){
             if(index == 1){
@@ -32,19 +35,19 @@ $(document).ready(function(){
                 return validateSecondStep();
             } else if(index == 3){
                 return validateThirdStep();
-            } //etc. 
-              
+            } //etc.
+
         },
         onTabClick : function(tab, navigation, index){
             // Disable the posibility to click on tabs
             return false;
-        }, 
+        },
         onTabShow: function(tab, navigation, index) {
             var $total = navigation.find('li').length;
             var $current = index+1;
-            
+
             var wizard = navigation.closest('.wizard-card');
-            
+
             // If it's the last tab then hide the last button and show the finish instead
             if($current >= $total) {
                 $(wizard).find('.btn-next').hide();
@@ -60,7 +63,7 @@ $(document).ready(function(){
     $("#wizard-picture").change(function(){
         readURL(this);
     });
-    
+
     $('[data-toggle="wizard-radio"]').click(function(){
         wizard = $(this).closest('.wizard-card');
         wizard.find('[data-toggle="wizard-radio"]').removeClass('active');
@@ -68,7 +71,7 @@ $(document).ready(function(){
         $(wizard).find('[type="radio"]').removeAttr('checked');
         $(this).find('[type="radio"]').attr('checked','true');
     });
-    
+
     $('[data-toggle="wizard-checkbox"]').click(function(){
         if( $(this).hasClass('active')){
             $(this).removeClass('active');
@@ -78,15 +81,15 @@ $(document).ready(function(){
             $(this).find('[type="checkbox"]').attr('checked','true');
         }
     });
-    
+
     $height = $(document).height();
     $('.set-full-height').css('height',$height);
-    
-    
+
+
 });
 
 function validateFirstStep(){
-    
+
     $(".wizard-card form").validate({
 		rules: {
 			firstname: "required",
@@ -95,7 +98,7 @@ function validateFirstStep(){
 				required: true,
 				email: true
 			}
-			
+
 /*  other possible input validations
 			,username: {
 				required: true,
@@ -110,13 +113,13 @@ function validateFirstStep(){
 				minlength: 5,
 				equalTo: "#password"
 			},
-		
+
 			topic: {
 				required: "#newsletter:checked",
 				minlength: 2
 			},
 			agree: "required"
-*/			
+*/
 
 		},
 		messages: {
@@ -142,42 +145,42 @@ function validateFirstStep(){
 			agree: "Please accept our policy",
 			topic: "Please select at least 2 topics"
 */
-				
+
 		}
-	}); 
-	
+	});
+
 	if(!$(".wizard-card form").valid()){
     	//form is invalid
     	return false;
 	}
-	
+
 	return true;
 }
 
 function validateSecondStep(){
-   
+
     //code here for second step
     $(".wizard-card form").validate({
 		rules: {
-			
+
 		},
 		messages: {
-			
+
 		}
-	}); 
-	
+	});
+
 	if(!$(".wizard-card form").valid()){
     	console.log('invalid');
     	return false;
 	}
 	return true;
-    
+
 }
 
 function validateThirdStep(){
     //code here for third step
-    
-    
+
+
 }
 
  //Function to show image before upload
@@ -192,16 +195,3 @@ function readURL(input) {
         reader.readAsDataURL(input.files[0]);
     }
 }
-    
-
-
-
-
-
-
-
-
-
-
-
-
