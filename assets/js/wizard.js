b5786b25e2bf7f5ab91d11ec54249a9e68863f75
Dragos Ct <dragosct10@gmail.com>
2016-08-19 15:41:51 +0300

    replace with new wizard js code

--- assets/js/wizard.js
+++ assets/js/wizard.js
@@ -8,72 +8,95 @@ $(document).ready(function(){
     /*  Activate the tooltips      */
     $('[rel="tooltip"]').tooltip();
 
-    $('.wizard-card').bootstrapWizard({
-        'tabClass': 'nav nav-pills',
-        'nextSelector': '.btn-next',
-        'previousSelector': '.btn-previous',
-
-         onInit : function(tab, navigation, index){
-
-           //check number of tabs and fill the entire row
-           var $total = navigation.find('li').length;
-           $width = 100/$total;
-           var $wizard = navigation.closest('.wizard-card');
-
-           $display_width = $(document).width();
-
-           if($display_width < 600 && $total > 3){
-               $width = 50;
-           }
-           console.log('aici intra in init');
-           refreshAnimation($wizard, index);
-
-           $('.moving-tab').css({
-               '-webkit-transition':'transform 0s'
-           });
-
-           navigation.find('li').css('width',$width + '%');
-
+    var $validator = $('.wizard-card form').validate({
+		  rules: {
+		    firstname: {
+		      required: true,
+		      minlength: 3
+		    },
+		    lastname: {
+		      required: true,
+		      minlength: 3
+		    },
+		    email: {
+		      required: true,
+		      minlength: 3,
+		    }
         },
-        onNext: function(tab, navigation, index){
-            if(index == 1){
-                return validateFirstStep();
-            } else if(index == 2){
-                return validateSecondStep();
-            } else if(index == 3){
-                return validateThirdStep();
-            } //etc.
 
-        },
-        onTabClick : function(tab, navigation, index){
-            // Disable the posibility to click on tabs
-            return false;
-        },
-        onTabShow: function(tab, navigation, index) {
-            var $total = navigation.find('li').length;
-            var $current = index+1;
+        errorPlacement: function(error, element) {
+            $(element).parent('div').addClass('has-error');
+         }
+		});
 
-            var $wizard = navigation.closest('.wizard-card');
+	  	$('.wizard-card').bootstrapWizard({
+            'tabClass': 'nav nav-pills',
+            'nextSelector': '.btn-next',
+            'previousSelector': '.btn-previous',
 
-            // If it's the last tab then hide the last button and show the finish instead
-            if($current >= $total) {
-                $($wizard).find('.btn-next').hide();
-                $($wizard).find('.btn-finish').show();
-            } else {
-                $($wizard).find('.btn-next').show();
-                $($wizard).find('.btn-finish').hide();
+            onNext: function(tab, navigation, index) {
+            	var $valid = $('.wizard-card form').valid();
+            	if(!$valid) {
+            		$validator.focusInvalid();
+            		return false;
+
+            	}
+            },
+
+            onInit : function(tab, navigation, index){
+
+              //check number of tabs and fill the entire row
+              var $total = navigation.find('li').length;
+              $width = 100/$total;
+              var $wizard = navigation.closest('.wizard-card');
+
+              $display_width = $(document).width();
+
+              if($display_width < 600 && $total > 3){
+                  $width = 50;
+              }
+              console.log('aici intra in init');
+              refreshAnimation($wizard, index);
+
+              $('.moving-tab').css({
+                  '-webkit-transition':'transform 0s'
+              });
+
+              navigation.find('li').css('width',$width + '%');
+
+           },
+
+            onTabClick : function(tab, navigation, index){
+                // Disable the posibility to click on tabs
+                return false;
+            },
+
+            onTabShow: function(tab, navigation, index) {
+                var $total = navigation.find('li').length;
+                var $current = index+1;
+
+                var $wizard = navigation.closest('.wizard-card');
+
+                // If it's the last tab then hide the last button and show the finish instead
+                if($current >= $total) {
+                    $($wizard).find('.btn-next').hide();
+                    $($wizard).find('.btn-finish').show();
+                } else {
+                    $($wizard).find('.btn-next').show();
+                    $($wizard).find('.btn-finish').hide();
+                }
+
+                button_text = navigation.find('li:nth-child(' + $current + ') a').html();
+
+                setTimeout(function(){
+                    $('.moving-tab').text(button_text);
+                }, 200);
+
+                console.log('aici in tabshow');
+                refreshAnimation($wizard, index);
             }
+	  	});
 
-            button_text = navigation.find('li:nth-child(' + $current + ') a').html();
-
-            setTimeout(function(){
-                $('.moving-tab').text(button_text);
-            }, 200);
-
-            console.log('aici in tabshow');
-            refreshAnimation($wizard, index);
-        }
-    });
 
     // Prepare the preview for profile picture
     $("#wizard-picture").change(function(){
@@ -102,103 +125,7 @@ $(document).ready(function(){
 
 });
 
-function validateFirstStep(){
 
-    $(".wizard-card form").validate({
-		rules: {
-			firstname: "required",
-			lastname: "required",
-			email: {
-				required: true,
-				email: true
-			}
-
-
-
-/*  other possible input validations
-			,username: {
-				required: true,
-				minlength: 2
-			},
-			password: {
-				required: true,
-				minlength: 5
-			},
-			confirm_password: {
-				required: true,
-				minlength: 5,
-				equalTo: "#password"
-			},
-
-			topic: {
-				required: "#newsletter:checked",
-				minlength: 2
-			},
-			agree: "required"
-*/
-
-		},
-		messages: {
-			firstname: "Please enter your First Name",
-			lastname: "Please enter your Last Name",
-			email: "Please enter a valid email address",
-
-/*   other posible validation messages
-			username: {
-				required: "Please enter a username",
-				minlength: "Your username must consist of at least 2 characters"
-			},
-			password: {
-				required: "Please provide a password",
-				minlength: "Your password must be at least 5 characters long"
-			},
-			confirm_password: {
-				required: "Please provide a password",
-				minlength: "Your password must be at least 5 characters long",
-				equalTo: "Please enter the same password as above"
-			},
-			email: "Please enter a valid email address",
-			agree: "Please accept our policy",
-			topic: "Please select at least 2 topics"
-*/
-
-		},
-        errorPlacement: function(error, element) {
-            $(element).parent('div').addClass('has-error');
-         }
-	});
-// '-webkit-transition':'all 500ms linear'
-	if(!$(".wizard-card form").valid()){
-    	//form is invalid
-    	return false;
-	}
-	return true;
-}
-
-function validateSecondStep(){
-
-    //code here for second step
-    $(".wizard-card form").validate({
-		rules: {
-
-		},
-		messages: {
-
-		}
-	});
-
-	if(!$(".wizard-card form").valid()){
-    	console.log('invalid');
-    	return false;
-	}
-	return true;
-
-}
-
-function validateThirdStep(){
-    //code here for third step
-
-}
 
  //Function to show image before upload
 
@@ -212,6 +139,9 @@ function readURL(input) {
         reader.readAsDataURL(input.files[0]);
     }
 }
+
+
+
 
 $(window).resize(function(){
     $('.wizard-card').each(function(){
